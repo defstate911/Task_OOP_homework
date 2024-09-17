dbd4a14d867ea9ef85d927ce87b7baef1f291ebc
defstate911 <9114404@gmail.com>
2024-09-17 10:09:05 +0300

    Update ex3.py

--- lesson_OOP_3/ex3.py
+++ lesson_OOP_3/ex3.py
@@ -5,3 +5,46 @@
 # каждый из которых переопределяет метод area().
 # В каждом из этих классов метод area() должен возвращать площадь соответствующей фигуры.
 # Написать функцию, которая принимает объект класса Shape и выводит его площадь.
+
+class Shape:
+    def area(self):
+        return 0
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius**2
+
+class Restangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side ** 2
+
+# p = Restangle(5, 10)
+# c = Circle(4)
+# k = Square(6)
+# print(p.area())
+# print(c.area())
+# print(k.area())
+
+def print_area(shape):
+    print(f'Площадь - {shape.area()}')
+
+circle = Circle(5)
+restangle = Restangle(5, 10)
+square = Square(7)
+print_area(circle)
+print_area(restangle)
+print_area(square)
