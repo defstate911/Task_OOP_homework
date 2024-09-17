518685b7bfaacabe71e6c5459ce907cb873a0cce
defstate911 <9114404@gmail.com>
2024-09-17 10:44:48 +0300

    add code

---
+++ lesson_OOP_3/ex4.py
@@ -0,0 +1,21 @@
+# Создайте класс Author и класс Book. Класс Book должен использовать композицию для включения
+# автора в качестве объекта.
+
+class Author:
+    def __init__(self, name, nationality):
+        self.name = name
+        self.nationality = nationality
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def get_info_book(self):
+        print(f'{self.title} - {self.author.name}')
+
+author = Author('Лев Толстой','Русская литература')
+book = Book('Война и мир', author)
+
+print(author.name)
+book.get_info_book()
